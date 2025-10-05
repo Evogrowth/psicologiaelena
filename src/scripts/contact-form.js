87b4fae218402c41155ebfd7c038d5cddfc4e53d
Carlos Garcia <carlosgarcia.dev@outlook.com>
2025-10-05 15:37:59 -0700

    Update contact form
    - Added email validation.
    - Added name attribute to phone number input.
    - Updated status message in contact form.

---
+++ src/scripts/contact-form.js
@@ -0,0 +1,106 @@
+import EmailValidator from 'email-validator';
+
+const fields = [
+    document.querySelector("#name-group"),
+    document.querySelector("#lastname-group"),
+    document.querySelector("#phone-number-group"),
+    document.querySelector("#email-group"),
+    document.querySelector("#service-group"),
+    document.querySelector("#birthdate-group")
+]
+
+fields.forEach(field => {
+    const input = field.querySelector("input");
+
+    input.addEventListener("input", function() {
+        field.classList.remove("invalid");
+        field.classList.remove("invalid-format");
+    });
+});
+
+const handleSubmit = async event => {
+    event.preventDefault();
+
+    let valid = true;
+
+    // Validar que todos los campos tengan información
+    for (let field of fields) {
+        const input = field.querySelector("input");
+
+        if (!input.value) {
+            field.classList.add("invalid");
+            valid = false;
+        }
+    }
+
+    // Evitar el envio del formuario si algún campo no tiene información
+    if (!valid) {
+        return;
+    }
+
+    // Validar que el número de contacto este completo
+    const phoneNumberGroup = document.querySelector("#phone-number-group");
+    const phoneNumberInput = phoneNumberGroup.querySelector("input");
+    if (phoneNumberInput.value.length != 14) {
+        phoneNumberGroup.classList.add("invalid-format");
+        return;
+    }
+
+    // Validar que el correo electrónico sea valido
+    const emailGroup = document.querySelector("#email-group");
+    const emailInput = emailGroup.querySelector("input");
+    if (!EmailValidator.validate(emailInput.value)) {
+        emailGroup.classList.add("invalid-format");
+        return;
+    }
+
+    const myForm = event.target;
+    const formData = new FormData(myForm);
+
+    const request = fetch("/", {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: new URLSearchParams(formData).toString()
+    });
+
+    // Abrir spinner de carga
+    Swal.fire({
+        position: 'top',
+        allowOutsideClick: false,
+        allowEscapeKey: false,
+        didOpen: () => {
+            Swal.showLoading();
+        }
+    });
+
+    const response = await request;
+
+    const swalConfig = {
+        position: 'top',
+        customClass: {
+            container: 'contact-form__swal-container',
+            htmlContainer: 'contact-form__swal-htmlContainer',
+            title: 'contact-form__swal-title'
+        },
+        showConfirmButton: false,
+        timer: 3000 // 3 segundos
+    }
+
+    if (response.ok) {
+        swalConfig.icon = 'success';
+        swalConfig.title = '¡Gracias por tu mensaje!';
+        swalConfig.text = 'Pronto te contactaré para agendar tu cita y resolver cualquier duda que tengas.';
+    }
+    else {
+        swalConfig.icon = 'error';
+        swalConfig.title = 'Ha ocurrido un problema.';
+        swalConfig.text = 'Lamento el inconveniente, pero tu mensaje no se ha enviado. Puedes volver a intentarlo en breve o contactarme por otro canal.';
+    }
+
+    // Cerrar spiner de carga
+    Swal.close();
+
+    Swal.fire(swalConfig);
+};
+
+document.querySelector("#contact-form").addEventListener("submit", handleSubmit);
